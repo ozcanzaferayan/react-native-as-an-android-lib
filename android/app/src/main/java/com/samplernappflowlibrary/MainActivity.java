524afbb83435a31ba193fcda6c2cc4b0a05e5670
Zafer AYAN <ozcanzaferayan@gmail.com>
2021-06-20 00:41:57 +0300

    Final touches

--- android/app/src/main/java/com/samplernappflowlibrary/MainActivity.java
+++ android/app/src/main/java/com/samplernappflowlibrary/MainActivity.java
@@ -1,15 +1,41 @@
 package com.samplernappflowlibrary;
+import android.app.Activity;
+import android.os.Bundle;
+import com.brentvatne.react.ReactVideoPackage;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.ReactRootView;
+import com.facebook.react.common.LifecycleState;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
 
-import com.facebook.react.ReactActivity;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends ReactActivity {
+public class MainActivity extends Activity {
+  private ReactRootView mReactRootView;
+  public static ReactInstanceManager mReactInstanceManager;
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
   @Override
-  protected String getMainComponentName() {
-    return "SampleRNAppFlowLibrary";
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    SoLoader.init(this, false);
+    mReactRootView = new ReactRootView(this);
+    List<ReactPackage> packages = new ArrayList<>();
+    packages.add(new MainReactPackage(null));
+    packages.add(new ReactVideoPackage());
+    mReactInstanceManager = ReactInstanceManager.builder()
+            .setApplication(getApplication())
+            .setCurrentActivity(this)
+            .setBundleAssetName("index.android.bundle")
+            .setJSMainModulePath("index")
+            .addPackages(packages)
+            .setUseDeveloperSupport(BuildConfig.DEBUG)
+            .setInitialLifecycleState(LifecycleState.RESUMED)
+            .build();
+    mReactRootView.startReactApplication(mReactInstanceManager,
+            "SampleRNAppFlowLibrary",
+            null);
+    setContentView(mReactRootView);
   }
 }
